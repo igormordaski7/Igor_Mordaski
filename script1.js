3a21c39053fa115a8aec0fb83c4180da4e2e2e45
igormordaski7 <144966766+igormordaski7@users.noreply.github.com>
2023-11-22 17:29:49 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,8 +1,3 @@
-/* Carrinho de compras */
-
-/* addEventListener está capturando os eventos de clique nos botões que foram adicionados */
-
-/* remover itens do carrinho */
 if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", ready);
 } else {
@@ -20,83 +15,7 @@ function ready() {
         button.addEventListener("click", adjustQuantity);
     });
 
-    const addToCartButtons = document.querySelectorAll(".add-to-cart"); // Alteração aqui
-    addToCartButtons.forEach((button) => {
-        button.addEventListener("click", () => addProductToCart(button));
-    });
 }
-
-/* para adicionar produto - testando
-
-function addProductToCart(event) {
-    console.log("addProductToCart called");
-    const button = event.target;
-
-    // Busca pelo elemento .product mais próximo
-    let productInfos = button.parentElement;
-
-    // Continua subindo no DOM até encontrar o elemento .product
-    while (productInfos && !productInfos.classList.contains("product")) {
-        productInfos = productInfos.parentElement;
-    }
-
-    if (!productInfos) {
-        console.error("Product information not found!");
-        return;
-    }
-
-    // Encontra os elementos dentro de productInfos
-    const productImage = productInfos.querySelector(".product-image");
-    const productTitleElement = productInfos.querySelector(".product-name");
-    const productPriceElement = productInfos.querySelector(".product-price");
-
-    if (!productImage || !productTitleElement || !productPriceElement) {
-        console.error("Product image, title, or price not found!");
-        return;
-    }
-
-    const productTitle = productTitleElement.innerText;
-    const productPrice = productPriceElement.innerText;
-
-    let newCartProduct = document.createElement("tr");
-    newCartProduct.classList.add("cart-product");
-
-    newCartProduct.innerHTML =
-        `
-        <td>
-        <div class="product">
-            <img src="${productImage.src}" alt="${productTitle}">
-            <div class="info">
-                <div class="name">${productTitle}</div>
-                <div class="category">Categoria</div>
-            </div>
-        </div>
-    </td>
-    <td class="product-value">${productPrice}</td>
-    <td>
-        <div class="qty">
-            <button><i class='bx bx-minus'></i></button>
-            <span class="product-quantify">1</span>
-            <button><i class='bx bx-plus'></i></button>
-        </div>
-    </td>
-    <td class="product-total">R$ ${productPrice}</td>
-    <td>
-        <button class="remove"><i class="bx bx-x"></i></button>
-    </td>
-    `;
-
-    const tableBody = document.querySelector(".cart-table tbody");
-
-    if (!tableBody) {
-        console.error("Table body not found!");
-        return;
-    }
-
-    tableBody.appendChild(newCartProduct);
-}
-*/
-
 
 function removeProduct(event) {
     const productContainer = event.target.parentElement.parentElement.parentElement;
